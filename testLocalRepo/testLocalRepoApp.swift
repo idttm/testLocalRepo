ed19a28f35cf4f62f022759d2b52320261ac0815
Andrii Cheberiako <andrew79911997@gmail.com>
2022-10-22 16:50:00 +0300

    Initial Commit

---
+++ testLocalRepo/testLocalRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  testLocalRepoApp.swift
+//  testLocalRepo
+//
+//  Created by Andrew Cheberyako on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct testLocalRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
